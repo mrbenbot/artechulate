be99c56cb84c776c662c5ec6a1e29954982af587
cwissy <meahchris@gmail.com>
2020-02-02 20:08:31 +0000

    start script and houdini refactor bits

--- public/assets/scripts/clock-worklet.js
+++ public/assets/scripts/clock-worklet.js
@@ -13,53 +13,55 @@ class ClockPainter {
   paint(ctx, size, props) {
     const totalSeconds = props.get("--total-seconds")[0];
     const time = props.get("--time")[0];
-    const width = props.get("--timer-width")[0];
-    const color = props.get("--timer-color-main")[0].trim();
-    const colorEnd = props.get("--timer-color-end")[0].trim();
+    const width = props.get("--timer-width")[0] || 3;
+    const color = props.get("--timer-color-main")[0].trim() || "hotpink";
+    const colorEnd = props.get("--timer-color-end")[0].trim() || "red";
     const lineType = props.get("--timer-line-type")[0].trim();
 
     const step = (Math.PI * 2) / totalSeconds;
-    ctx.lineWidth = width || 3;
-    ctx.strokeStyle = time > 5 ? color || "hotpink" : colorEnd || "red";
+    ctx.lineWidth = width;
+    ctx.strokeStyle = time > 5 ? color : colorEnd;
 
     switch (lineType) {
       case "dashed":
-        this.drawDashed({ time, size, step, ctx });
+        drawDashed({ time, size, step, ctx });
         break;
       case "solid":
       default:
-        this.drawSolid({ totalSeconds, time, size, step, ctx });
+        drawSolid({ totalSeconds, time, size, step, ctx });
         break;
     }
   }
-  drawDashed({ time, size, step, ctx }) {
-    for (let i = 0; i < time; i++) {
-      ctx.beginPath();
-      ctx.arc(
-        size.width / 2,
-        size.height / 2,
-        size.height / 3,
-        Math.PI * -0.5 + i * step,
-        Math.PI * -0.5 + i * step + step / 4,
-        false
-      );
-      ctx.closePath();
-      ctx.stroke();
-    }
-  }
-  drawSolid({ totalSeconds, time, step, ctx, size }) {
-    const inverseTime = totalSeconds - time;
+}
+
+function drawDashed({ time, size, step, ctx }) {
+  for (let i = 0; i < time; i++) {
     ctx.beginPath();
     ctx.arc(
       size.width / 2,
       size.height / 2,
       size.height / 3,
-      Math.PI * -0.5,
-      Math.PI * 1.5 - inverseTime * step,
+      Math.PI * -0.5 + i * step,
+      Math.PI * -0.5 + i * step + step / 4,
       false
     );
+    ctx.closePath();
     ctx.stroke();
   }
+}
+
+function drawSolid({ totalSeconds, time, step, ctx, size }) {
+  const inverseTime = totalSeconds - time;
+  ctx.beginPath();
+  ctx.arc(
+    size.width / 2,
+    size.height / 2,
+    size.height / 3,
+    Math.PI * -0.5,
+    Math.PI * 1.5 - inverseTime * step,
+    false
+  );
+  ctx.stroke();
 }
 
 registerPaint("clock-circle", ClockPainter);
